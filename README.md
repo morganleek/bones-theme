0456b3a08ab40822a2f52268c86705105d7e37fa
Morgan Leek <morganleek@gmail.com>
2022-11-16 10:53:15 +0800

    readme ftp

--- README.md
+++ README.md
@@ -30,3 +30,15 @@ npm run build
 ```bash
 npm run archive
 ```
+
+## Git FTP
+
+Set Git FTP settings with 'syncroot' pointing at packages directory. You may need to export any templates if these have been edited in WP.
+
+```[git-ftp]
+	url = ftpes://1.1.1.1/public_html/wp-content/themes/{directory_name}
+	user = "username"
+	password = "password"
+	insecure = 1
+	syncroot = package/{directory_name}
+```
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'fc990b1542a3f5aa6fe93cc2060db09196bc631b',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fc990b1542a3f5aa6fe93cc2060db09196bc631b',
+    ),
+    'wpackio/enqueue' => 
+    array (
+      'pretty_version' => '3.4.0',
+      'version' => '3.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8a5604f5ab3bd5f000e503dda02603e15e534d47',
+    ),
+  ),
+);
