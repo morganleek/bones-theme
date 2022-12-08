33f04921f9aea1c747061fbf0b054ded669fb3f4
Morgan Leek <morganleek@gmail.com>
2022-12-08 10:56:34 +0800

    favicon

--- functions.php
+++ functions.php
@@ -37,6 +37,7 @@
 			add_action( 'after_setup_theme', [ $this, 'bones_theme_support' ] );
 			add_action( 'admin_init', [$this, 'bones_theme_editor_styles' ] );
 			add_action( 'wp_head', [ $this, 'bones_theme_preload_webfonts' ] );
+			add_action( 'wp_head', [ $this, 'bones_theme_load_favicons' ] );
 			add_action( 'init', [ $this, 'bones_name_register_block_styles' ], 100 );
 
 			// Gallery Sliders
@@ -83,6 +84,11 @@
 					add_editor_style( $url );
 				}
 			}
+		}
+
+		public function bones_theme_load_favicons() {
+			print '<link rel="icon" href="' . get_theme_file_uri( 'assets/favicon/favicon.ico' ) . '" sizes="any">';
+			print '<link rel="icon" href="' . get_theme_file_uri( 'assets/favicon/favicon.svg' ) . '" type="image/svg+xml">';
 		}
 
 		public function bones_theme_editor_styles() {
