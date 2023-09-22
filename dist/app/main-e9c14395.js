dd581c3676f0e3e41320bd52f8cd26cdf3bece22
Morgan Leek <morganleek@gmail.com>
2023-09-22 16:25:12 +0800

    remove bloat + variations

---
+++ dist/app/main-e9c14395.js
@@ -0,0 +1,19 @@
+/*!
+ * 
+ * bonesTheme
+ * 
+ * @author 
+ * @version 0.1.0
+ * @link UNLICENSED
+ * @license UNLICENSED
+ * 
+ * Copyright (c) 2023 
+ * 
+ * This software is released under the UNLICENSED License
+ * https://opensource.org/licenses/UNLICENSED
+ * 
+ * Compiled with the help of https://wpack.io
+ * A zero setup Webpack Bundler Script for WordPress
+ */
+(window.wpackiobonesThemeappJsonp=window.wpackiobonesThemeappJsonp||[]).push([[0],[function(e,n,o){o(1),e.exports=o(2)},function(e,n,o){var a="bonesThemedist".replace(/[^a-zA-Z0-9_-]/g,"");o.p=window["__wpackIo".concat(a)]},function(e,n,o){"use strict";o.r(n),document.addEventListener("DOMContentLoaded",(function(){document.querySelectorAll('img[loading="lazy"]').forEach((function(e){!0===e.complete&&e.classList.add("has-loaded"),e.addEventListener("load",(function(e){e.target.classList.add("has-loaded")}))}))}))}],[[0,1]]]);
+//# sourceMappingURL=main-e9c14395.js.map
